6db8f07e9c02e788c69c2b5e06d26c7d81dca44e
mikigoalie <mikigoalie@seznam.cz>
2023-04-05 23:16:41 +0200

    update(server, locales): add fr locales, remove prints, fix esx bank
    locales by Bobbber#5545

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,4 @@
 local _inv = exports.ox_inventory
-local _pefcl = exports.pefcl
 
 lib.callback.register('lsrp_vehicleshop:setInstance', function(source, entered)
     SetPlayerRoutingBucket(source, entered and source or 0)
@@ -22,7 +21,7 @@ lib.callback.register('lsrp_vehicleShop:server:payment', function(source, useBan
     end
 
     local vehiclePrice = Config.vehicleList[Config.vehicleShops[_shopIndex].vehicleList][_selected].values[_secondary].vehiclePrice
-    print(vehiclePrice)
+
     if not tonumber(vehiclePrice) or vehiclePrice < 1000 then return false end
 
     if not useBank then
@@ -34,14 +33,20 @@ lib.callback.register('lsrp_vehicleShop:server:payment', function(source, useBan
         return _inv:RemoveItem(source, 'money', vehiclePrice)
     end
 
-    local bankMoney = _pefcl:getDefaultAccountBalance(source).data
-    print(bankMoney)
+
+    local bankMoney = Config.usePEFCL and exports.pefcl:getDefaultAccountBalance(source).data or xPlayer.getAccount('bank').money
     if bankMoney < vehiclePrice then
         return false
     end
 
-    local result = _pefcl:removeBankBalance(source, { amount = vehiclePrice, message = ('Zakoupení vozidla %s'):format(Config.vehicleList[Config.vehicleShops[_shopIndex].vehicleList][_selected].values[_secondary].label) })
-    return result.status == 'ok' or false
+    if Config.usePEFCL then
+        local result = exports.pefcl:removeBankBalance(source, { amount = vehiclePrice, message = ('Zakoupení vozidla %s'):format(Config.vehicleList[Config.vehicleShops[_shopIndex].vehicleList][_selected].values[_secondary].label) })
+        return result.status == 'ok' or false
+    else
+        xPlayer.removeAccountMoney('bank', vehiclePrice)
+        return true
+    end
+
 end)
 
 local function getPlate()
@@ -62,21 +67,17 @@ lib.callback.register('lsrp_vehicleShop:server:addVehicle', function(source, veh
     local coords = xPlayer.getCoords(true)
     local _vehProps = vehProperties
 
-    print(('VEH MODELS %s %s'):format(_vehProps.model, data.vehicleModel))
     if _vehProps.model ~= data.vehicleModel then
         return false
     end
-    print(vehicleSpot)
 
     local alreadyExists = MySQL.single.await('SELECT owner FROM owned_vehicles WHERE plate = ?', {vehProperties.plate})
-    print(('DB EXIST? %s'):format(alreadyExists))
 
     if alreadyExists?.owner then
         _vehProps.plate = getPlate()
     end
 
     local success = MySQL.insert.await('INSERT INTO owned_vehicles (owner, plate, vehicle, stored, type) VALUES (?, ?, ?, ?, ?)', {xPlayer.identifier, _vehProps.plate, json.encode(_vehProps), vehicleSpot ~= 0, Config.vehicleList[Config.vehicleShops[_shopIndex].vehicleList][_selected].dbData})
-    print(('DSUICCCESS? %s'):format(success))
     if vehicleSpot == 0 then
         ESX.OneSync.SpawnVehicle(data.vehicleModel, Config.vehicleShops[_shopIndex].vehicleSpawnCoords.xyz, Config.vehicleShops[_shopIndex].vehicleSpawnCoords.w, _vehProps, function(NetworkId)
             Wait(100)
